38a4c6e03389cd59e06245479867d1952c337f8d
James Beuzeval <james.beuzeval@gmail.com>
2025-07-06 10:49:22 +0100

    Still troubleshooting

--- custom_components/jersey_weather/camera.py
+++ custom_components/jersey_weather/camera.py
@@ -27,6 +27,7 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ) -> None:
+    _LOGGER.error("<<<<< LOADING NEW CAMERA.PY - IF YOU SEE THIS, THE FILE IS UPDATED >>>>>")
     """Set up Jersey Weather camera entities based on config entry."""
     cameras = [
         JerseyWeatherRadarCamera(hass, "radar", "Radar"),
